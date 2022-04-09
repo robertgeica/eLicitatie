07d80212389c7a7f8e2449d4284737083c8c79a6
robertgeica <geicarobert@gmail.com>
2022-04-09 23:00:40 +0300

    render and filter auctions

--- frontend/assets/javascript/app.js
+++ frontend/assets/javascript/app.js
@@ -80,3 +80,86 @@ if (store().user.role === "admin") {
   const authContainer = document.getElementById("auth");
   authContainer.innerHTML = adminLink + authContainer.innerHTML;
 }
+
+const isHomepage = window.location.pathname === "/frontend/index.html";
+// renderCard
+const renderCard = (auction) => {
+  const category = auction.categories.map(
+    (category) => ` <span>${category}</span>`
+  );
+  return `
+  <div class="auction-container">
+    <img src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" width="400" height="300" alt="photo" />
+    <div class="text-content">
+    <h2>${auction.name}</h2>
+    <h4>${category}</h4>
+    <div style="display: flex">
+      <span style="margin-right: 10px">Last bid</span>
+      <h4>${0}</h4>
+    </div>
+    <p>${auction.endDate}</p>
+    </div>
+  </div>
+  `;
+};
+
+const auctionCategoriesFilter = document.getElementById(
+  "auctions-category-select"
+);
+const renderAuctions = (isFiltered, selectedCategory) => {
+  const auctions =
+    isFiltered && selectedCategory.length !== 0 ? isFiltered : store().products;
+  auctionCategoriesFilter.innerHTML = "";
+
+  store().categories.forEach((category, index) => {
+    category.subcategories.forEach((subcategory) => {
+      const optionSubcategory = `<option value="${subcategory}" ${
+        subcategory === selectedCategory ? "selected" : ""
+      }>---${subcategory}</option>`;
+      auctionCategoriesFilter.innerHTML =
+        optionSubcategory + auctionCategoriesFilter.innerHTML;
+    });
+
+    const optionCategory = `<option value="${category.name}" ${
+      category.name === selectedCategory ? "selected" : ""
+    }>${category.name}</option>`;
+    auctionCategoriesFilter.innerHTML =
+      optionCategory + auctionCategoriesFilter.innerHTML;
+  });
+
+  const cards = auctions.map((auction) => renderCard(auction));
+
+  return cards.join("");
+};
+
+const filterAuctions = () => {
+  auctionCategoriesFilter.onchange = (e) => {
+    const auctions = store().products;
+    const selectedCategory = e.target.value;
+    let newAuctions = auctions.map((product) => {
+      const hasCategory = product.categories.filter(
+        (category) => category === auctionCategoriesFilter.value
+      );
+
+      if (hasCategory.length > 0) {
+        return product;
+      }
+    });
+
+    newAuctions = newAuctions.filter(
+      (product) => typeof product !== "undefined"
+    );
+
+    document.getElementById("auctions").innerHTML = renderAuctions(
+      newAuctions,
+      selectedCategory.trim()
+    );
+  };
+};
+
+if (isHomepage) {
+  const auctionsContainer = document.getElementById("auctions");
+  auctionsContainer.innerHTML = renderAuctions();
+
+  filterAuctions();
+}
