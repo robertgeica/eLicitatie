1c71945e36c740efbb1d3f2aa3212e44cee8d0fe
robertgeica <geicarobert@gmail.com>
2022-04-12 21:16:05 +0300

    update product schema

--- Models/ProductModel.cs
+++ Models/ProductModel.cs
@@ -25,4 +25,6 @@ public class Offers
   public double offerId { get; set; }
   public string userId { get; set; }
   public string value { get; set; }
+  public string userName { get; set; }
+  public string date { get; set; }
 }
